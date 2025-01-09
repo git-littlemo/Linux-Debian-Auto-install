275b027c24637a7c8b04e10a2e23a6ec072ce660
Littlemo <git@littlemo.cc>
2025-01-09 17:58:32 +0800

    更新和优化
    1. 显性指定要使用的磁盘，而不是自动使用第一块
    2. 启用了网络控制台，以便查看安装过程，以及出现意外时认为操作

--- preseed.sh
+++ preseed.sh
@@ -25,20 +25,17 @@ d-i partman-auto/choose_recipe select boot-root
 d-i partman-auto/expert_recipe string                         \
       boot-root ::                                            \
               512 512 1024 free                               \
-                      $iflabel{ gpt }                         \
-                      $reusemethod{ }                         \
-                      method{ efi }                           \
-                      format{ }                               \
+                      method{ efi } format{ }                 \
+                      filesystem{ vfat }                      \
+                      mountpoint{ /boot/efi }                 \
               .                                               \
               512 512 1024 ext4                               \
                       $primary{ } $bootable{ }                \
-                      method{ format } format{ }              \
-                      use_filesystem{ } filesystem{ ext4 }    \
+                      method{ format } filesystem{ ext4 }     \
                       mountpoint{ /boot }                     \
               .                                               \
               1000 10000 1000000000 ext4                      \
-                      method{ format } format{ }              \
-                      use_filesystem{ } filesystem{ ext4 }    \
+                      method{ format } filesystem{ ext4 }     \
                       mountpoint{ / }                         \
               .                                               \
               512 1024 200% linux-swap                        \
@@ -63,17 +60,15 @@ d-i partman-auto/choose_recipe select boot-root
 d-i partman-auto/expert_recipe string                         \
       boot-root ::                                            \
               1 1 1 free                                      \
-                      method{ bios_grub }                    \
+                      method{ bios_grub }                     \
               .                                               \
               512 512 1024 ext4                               \
                       $primary{ } $bootable{ }                \
-                      method{ format } format{ }              \
-                      use_filesystem{ } filesystem{ ext4 }    \
+                      method{ format } filesystem{ ext4 }     \
                       mountpoint{ /boot }                     \
               .                                               \
               1000 10000 1000000000 ext4                      \
-                      method{ format } format{ }              \
-                      use_filesystem{ } filesystem{ ext4 }    \
+                      method{ format } filesystem{ ext4 }     \
                       mountpoint{ / }                         \
               .                                               \
               512 1024 200% linux-swap                        \
@@ -105,14 +100,12 @@ d-i partman-auto/expert_recipe string                         \
       boot-root ::                                            \
               512 512 1024 ext4                               \
                       $primary{ } $bootable{ }                \
-                      method{ format } format{ }              \
-                      use_filesystem{ } filesystem{ ext4 }    \
+                      method{ format } filesystem{ ext4 }     \
                       mountpoint{ /boot }                     \
               .                                               \
               1000 10000 1000000000 ext4                      \
                       $primary{ }                             \
-                      method{ format } format{ }              \
-                      use_filesystem{ } filesystem{ ext4 }    \
+                      method{ format } filesystem{ ext4 }     \
                       mountpoint{ / }                         \
               .                                               \
               512 1024 200% linux-swap                        \
@@ -129,33 +122,47 @@ fi
 
 preseed="""
 # 预配置文件
+
 # 低内存模式
 d-i lowmem/low boolean true
 d-i lowmem/insufficient boolean true
+
 # 语言和地区
 d-i debian-installer/locale string en_US.UTF-8
 d-i debian-installer/country string US
 d-i debian-installer/language string en
 d-i keyboard-configuration/xkb-keymap select us
+
+# 设置时区
+d-i clock-setup/utc boolean true
+d-i time/zone string Asia/Hong_Kong
+
 # 网络设置
 d-i netcfg/choose_interface select ${interface}
 d-i netcfg/get_hostname string debian
 d-i netcfg/get_nameservers string 8.8.8.8
+
+# 启用网络控制台模块
+d-i anna/choose_modules string network-console
+# 设置网络控制台的密码，默认用户名： installer
+d-i network-console/password password ${root_pass}
+d-i network-console/password-again password ${root_pass}
+
 # 设置镜像源
 d-i mirror/country string manual
 d-i mirror/http/hostname string ${mirror_domain}
 d-i mirror/http/directory string /debian
 d-i mirror/http/proxy string
-# 设置时区
-d-i clock-setup/utc boolean true
-d-i time/zone string Asia/Hong_Kong
+
 # 分区设置
 d-i partman-auto/disk string ${boot_device}
 ${partman}
+
 # 设置root用户密码
 d-i passwd/root-login boolean true
 d-i passwd/root-password-crypted password ${root_pass}
 d-i passwd/make-user boolean false
+
 # 配置apt和软件选择
 tasksel tasksel/first multiselect standard
 d-i pkgsel/include string openssh-server build-essential
